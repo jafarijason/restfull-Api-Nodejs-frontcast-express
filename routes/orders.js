1ba69a4fda8e1de514f6dd625abacf0eb2bb5dda
Jason <jason.jafari.m@gmail.com>
2020-07-07 13:59:27 -0400

    make route to add data to database

--- routes/orders.js
+++ routes/orders.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const router = express.Router();
 
+const mongoose = require("mongoose"); // We want use Object Id for generating Id
+
+const Order = require("../models/order");
+
 router.get("/", (req, res, next) => {
   res.status(200).json({
     msg: "GET   orders",
@@ -9,14 +13,30 @@ router.get("/", (req, res, next) => {
 });
 
 router.post("/", (req, res, next) => {
-  const order ={
-    productId : req.body.productId,
-    qty: req.body.qty
-  }
-  res.status(200).json({
-    msg: "POST   orders",
-    order:order
+  const order = new Order({
+    _id: mongoose.Types.ObjectId(),
+    product: req.body.productId,
+    qty: req.body.qty,
   });
+  order
+    .save()
+    .then((result) => {
+      console.log(result);
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
+
+  // const order = {
+  //   productId: req.body.productId,
+  //   qty: req.body.qty,
+  // };
+  // res.status(200).json({
+  //   msg: "POST   orders",
+  //   order: order,
+  // });
 });
 
 router.get("/:orderId", (req, res, next) => {
@@ -26,14 +46,11 @@ router.get("/:orderId", (req, res, next) => {
   });
 });
 
-
-
 router.delete("/:orderId", (req, res, next) => {
   res.status(200).json({
     msg: "Delete Order",
     orderId: req.params.orderId,
   });
 });
-
 
 module.exports = router;
